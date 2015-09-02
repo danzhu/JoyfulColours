9038a8fbe4951ff8f16ab62e7142e6f585e09b32
Daniel Zhu <danielzhu2497@hotmail.com>
2015-09-03 04:23:03 +0800

    Added startup script to project

---
+++ JoyfulColours/startup.py
@@ -0,0 +1,12 @@
+﻿if False:
+    import clr
+    clr.AddReference('JoyfulColours')
+    clr.AddReference('PresentationCore')
+    clr.AddReference('PresentationFramework')
+    from __builtin__ import *
+from JoyfulColours import *
+from JoyfulColours.Procedures import *
+
+Game.LoadLooseFiles('data')
+#Game.LoadObjects()
+Game.LoadQueued()
